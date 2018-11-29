f0c05225d8b3a82eb052f26ea97ade19bed87e44
cazerb <45408623+cazerb@users.noreply.github.com>
2018-11-29 15:29:35 -0500

    Add files via upload

--- Finalv1.c
+++ Finalv1.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <sys / mman.h>
-#include <sys / stat.h>
-#include <sys / fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
 
-#define KEYSIZE 8;
-#define DATASIZE 56;
+#define KEYSIZE 8
+#define DATASIZE 56
 
 typedef struct P*{
 	int threadNum;
 	int size;
 	int startIndex;
-}ThreadParam;
+}threadParam;
+
+typedef struct Record {
+	char key[KEYSIZE];
+	char data[DATASIZE];
+};
 
 void * threadFunc(void*);
 
@@ -20,18 +25,20 @@ int compareFunc();
 
 int main()
 {
+	printf("Starting\n");
 	//Initialize variables: var & init (precord where file mapped,pointer to thr arg,#rec,recsize,array of tid's)etc
 	
 	char * addr;
 	int fd;
 	struct stat sb; 
 
-	
 	//get number of cores
 	//get number of threads to run off of, throw error if greater than cores
-	numThreads = 2;
+	int numThreads = 2;
 
+	pthread_t threadID[numThreads];
 	threadParam Params[numThreads];
+
 	//Final test may be a string
 	//May be read and write
 	//O_RDONLY (Read only)
@@ -50,18 +57,19 @@ int main()
 	}
 	
 	addr = mmap(NULL, sb.st_size, PROT_WRITE, MAP_PRIVATE, fd, 0);
-
 	if (addr == MAP_FAILED)
 	{
 		printf("mmap failed \n");
 		exit(EXIT_FAILURE);
 	}
-	
+
+	printf("File loaded\n");
+
 	for (int i = 0; i < numThreads; i++)
 	{
 		Params[i].size = sb.st_size / numThreads;
 		Params[i].startIndex = addr + (Params[i].size *i);
-		Params[i].ThreadNum = i;
+		Params[i].threadNum = i;
         if (pthread_create(&threadID[i], NULL, threadFunc, &Params[i]))
         {             
 			printf("Thread create failed \n");
@@ -72,13 +80,14 @@ int main()
 
 void * threadFunc(void *param)
 {
+	printf("Starting threadFunc\n");
 	threadParam *params = (threadParam *)param;
 
-	qsort(params->startIndex , KEYSIZE+DATASIZE, KEYSIZE, compareFunc));
-
+	qsort(params->startIndex , KEYSIZE+DATASIZE, KEYSIZE, compareFunc);
+	printf("Sort Complete\n");
 }
 
 int compareFunc(const void *a, const void *b)
 {
-	return (*(int*)a - *(int*)b);
+	return (*(long*)a - *(long*)b);
 }
